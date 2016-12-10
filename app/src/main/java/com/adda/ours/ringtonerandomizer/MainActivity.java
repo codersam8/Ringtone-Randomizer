21a94c1ba39bfc5c7ba510922cecff0bceeb5973
sampath <sampath.skt@gmail.com>
2016-12-10 14:14:16 +0530

    app/src/main/java/com/adda/ours/ringtonerandomizer/MainActivity.java

--- app/src/main/java/com/adda/ours/ringtonerandomizer/MainActivity.java
+++ app/src/main/java/com/adda/ours/ringtonerandomizer/MainActivity.java
@@ -1,5 +1,8 @@
 package com.adda.ours.ringtonerandomizer;
 
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Ringtone defaultRingtone = RingtoneManager.getRingtone(getApplicationContext(),
+                Settings.System.DEFAULT_RINGTONE_URI);
+        defaultRingtone.play();
     }
 }
