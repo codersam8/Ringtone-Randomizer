446366217ae3b55b69a0ec322cafcb43dc5836e7
sampath <sampath.skt@gmail.com>
2016-12-25 13:58:18 +0530

    added new  file

--- app/src/main/java/com/adda/ours/ringtonerandomizer/MainActivity.java
+++ app/src/main/java/com/adda/ours/ringtonerandomizer/MainActivity.java
@@ -1,7 +1,10 @@
 package com.adda.ours.ringtonerandomizer;
 
+import android.database.Cursor;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,5 +19,33 @@ public class MainActivity extends AppCompatActivity {
         Ringtone defaultRingtone = RingtoneManager.getRingtone(getApplicationContext(),
                 Settings.System.DEFAULT_RINGTONE_URI);
         defaultRingtone.play();
+//        test code for projection
+        String[] projection = new String[] {
+                MediaStore.Audio.AudioColumns.ALBUM,
+                MediaStore.Audio.AudioColumns.TITLE };
+        Uri contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        Cursor cursor = getContentResolver().query(contentUri,
+                projection, null, null, null);
+        // Get the index of the columns we need.
+        int albumIdx = cursor
+                .getColumnIndexOrThrow(MediaStore.Audio.AudioColumns.ALBUM);
+        int titleIdx = cursor
+                .getColumnIndexOrThrow(MediaStore.Audio.AudioColumns.TITLE);
+        // Create an array to store the result set.
+        String[] result = new String[cursor.getCount()];
+                /*
+                 * Iterate over the Cursor, extracting each album name and song
+                 * title.
+                 */
+        while (cursor.moveToNext()) {
+            // Extract the song title.
+            String title = cursor.getString(titleIdx);
+            // Extract the album name.
+            String album = cursor.getString(albumIdx);
+            result[cursor.getPosition()] = title + " (" + album + ")";
+        }
+        // Close the Cursor.
+        cursor.close();
+
     }
 }
