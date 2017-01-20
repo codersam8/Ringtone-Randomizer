21a8641d7b820068bd690d7cb4218e9ab7fb7fc9
ssurinen <sampath.surineni@oracle.com>
2017-01-21 00:43:19 +0530

    added missing files

---
+++ app/src/main/java/com/adda/ours/ringtonerandomizer/CallHelper.java
@@ -0,0 +1,84 @@
+package com.adda.ours.ringtonerandomizer;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Random;
+
+/**
+ * Helper class to detect incoming and outgoing calls.
+ *
+ * @author Moskvichev Andrey V.
+ */
+public class CallHelper {
+    private static final String TAG = "CallHelper";
+    /**
+     * Listener to detect incoming calls.
+     */
+    private class CallStateListener extends PhoneStateListener {
+        @Override
+        public void onCallStateChanged(int state, String incomingNumber) {
+            switch (state) {
+                case TelephonyManager.CALL_STATE_RINGING:
+                    // called when someone is ringing to this phone
+                    chooseARandomTone();
+                    Toast.makeText(ctx,
+                            "Incoming: " + incomingNumber,
+                            Toast.LENGTH_LONG).show();
+                    break;
+            }
+        }
+    }
+    private void chooseARandomTone() {
+        songsList = ctx.getSharedPreferences("SongsList", ctx.MODE_PRIVATE);
+        Map<String, String> songsMap = (Map<String, String>)songsList.getAll();
+        Collection<String> songsColl = (Collection<String>) songsMap.values();
+        String[] songListArr = new String[songsColl.size()];
+        songsColl.toArray(songListArr);
+//        for(String song: songListArr) {
+//            Log.i(TAG, song);
+//        }
+        RingtoneManager.setActualDefaultRingtoneUri(ctx,
+                RingtoneManager.TYPE_RINGTONE,
+                Uri.parse(songListArr[new Random().nextInt(songListArr.length)]));
+    }
+    private SharedPreferences songsList;
+    private Context ctx;
+    private TelephonyManager tm;
+    private CallStateListener callStateListener;
+
+    public CallHelper(Context ctx) {
+        this.ctx = ctx;
+
+        callStateListener = new CallStateListener();
+    }
+
+    /**
+     * Start calls detection.
+     */
+    public void start() {
+        tm = (TelephonyManager) ctx.getSystemService(Context.TELEPHONY_SERVICE);
+        tm.listen(callStateListener, PhoneStateListener.LISTEN_CALL_STATE);
+
+        IntentFilter intentFilter = new IntentFilter(Intent.ACTION_NEW_OUTGOING_CALL);
+    }
+
+    /**
+     * Stop calls detection.
+     */
+    public void stop() {
+        tm.listen(callStateListener, PhoneStateListener.LISTEN_NONE);
+    }
+
+}
