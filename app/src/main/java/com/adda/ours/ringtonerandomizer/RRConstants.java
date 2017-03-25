e1ab4abedbbbf2b11e2806a5fb8d25d3d0eaacef
ssurinen <sampath.surineni@oracle.com>
2017-03-25 15:49:50 +0530

    added constats interface

---
+++ app/src/main/java/com/adda/ours/ringtonerandomizer/RRConstants.java
@@ -0,0 +1,10 @@
+package com.adda.ours.ringtonerandomizer;
+
+/**
+ * Created by ssurinen on 3/25/2017.
+ */
+
+interface RRConstants {
+    String ON_STATE_TEXT = "STOP RANDOMIZE";
+    String OFF_STATE_TEXT = "RANDOMIZE";
+}
