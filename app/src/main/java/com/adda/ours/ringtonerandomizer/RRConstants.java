c44fd93b1997bde9f8a198ac1616a92b678bdc76
sampath <sampath.surineni@oracle.com>
2017-06-03 14:55:08 +0530

    added rr constants

---
+++ app/src/main/java/com/adda/ours/ringtonerandomizer/RRConstants.java
@@ -0,0 +1,10 @@
+package com.adda.ours.ringtonerandomizer;
+
+/**
+ * Created by ssurinen on 3/25/2017.
+ */
+
+public class RRConstants {
+    public static final String ON_STATE_TEXT = "STOP RANDOMIZE";
+    public static final String OFF_STATE_TEXT = "RANDOMIZE";
+}
